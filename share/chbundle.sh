d6ba11d60dac63e8b53536dd4257a2f3bf215140
Steve Loveless <steve.loveless@gmail.com>
2014-06-11 21:00:42 -0700

    Added comments and fixed indentation

--- share/chbundle.sh
+++ share/chbundle.sh
@@ -5,56 +5,64 @@ function chbundle_list()
     local_shell=`ps -o command -p $$`
 
     case "$local_shell" in
-      *zsh)
-        setopt extendedglob
-        gemfiles=`ls -d Gemfile^*lock`
-        ;;
-      *bash)
-        gemfiles=`GLOBIGNORE="*.lock" && ls Gemfile*`
-        ;;
-      *)
-        echo "Not sure how to behave with this shell: $local_shell"
-        return 1
-        ;;
+        *zsh)
+            setopt extendedglob
+            gemfiles=`ls -d Gemfile^*lock`
+            ;;
+        *bash)
+            gemfiles=`GLOBIGNORE="*.lock" && ls Gemfile*`
+            ;;
+        *)
+            echo "Not sure how to behave with this shell: $local_shell"
+            return 1
+            ;;
     esac
 
     bundles=`echo "$gemfiles" | cut -f2 -d"."`
     echo "chbundle  version $CHBUNDLE_VERSION"
     echo "bundles:"
 
+    # Print bundles if there are any.
     if [[ -n $bundles ]]
     then
-      for bundle in $(echo "$bundles")
-      do
-        if [[ "$bundle" == "Gemfile" ]]
-        then
-          bundle_name="default"
+        for bundle in $(echo "$bundles")
+        do
+            # Name the bundle 'default' if "Gemfile" is found.
+            if [[ "$bundle" == "Gemfile" ]]
+            then
+                bundle_name="default"
 
-          if [[ -e $BUNDLE_GEMFILE ]]
-          then
-            echo "envvar set"
-            if [[ "Gemfile" == "$BUNDLE_GEMFILE" ]]
-              then
-              prefix="*-"
+                # If Bundler's ENV var is set...
+                if [[ -e $BUNDLE_GEMFILE ]]
+                then
+                    # ...and it's set to "Gemfile"...
+                    if [[ "Gemfile" == "$BUNDLE_GEMFILE" ]]
+                    then
+                        # then show that it's set.
+                        prefix="*-"
+                    else
+                        # otherwise just show that it exists
+                        prefix=" -"
+                    fi
+                # If Bundler's ENV var isn't set, show that "Gemfile" is default.
+                else
+                    prefix="*-"
+                fi
             else
-              prefix=" -"
+                bundle_name=$bundle
+
+                # Show the currently set bundle.
+                if [[ "$BUNDLE_GEMFILE" == *"$bundle" ]]
+                then
+                    prefix="*-"
+                else
+                    prefix=" -"
+                fi
             fi
-          else
-            prefix="*-"
-          fi
-        else
-          bundle_name=$bundle
 
-          if [[ "$BUNDLE_GEMFILE" == *"$bundle" ]]
-          then
-            prefix="*-"
-          else
-            prefix=" -"
-          fi
-        fi
-
-        echo "$prefix $bundle_name"
-      done
+            echo "$prefix $bundle_name"
+        done
+    # Otherwise, let em know we didn't find any bundles.
     else
         echo "No bundles found."
     fi

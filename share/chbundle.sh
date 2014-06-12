59cce67aa9c20d2fa712270d9f2d54be40f26dc9
Steve Loveless <steve.loveless@gmail.com>
2014-06-11 19:56:24 -0700

    Made nicer for zsh and bash

--- share/chbundle.sh
+++ share/chbundle.sh
@@ -2,13 +2,34 @@ CHBUNDLE_VERSION="0.1.0"
 
 function chbundle_list()
 {
-    bundles=`ls Gemfile*[^lock] | cut -f2 -d"."`
+    local_shell=`ps -o command -p $$`
+    # If zshell
+    case "$local_shell" in
+      *zsh)
+        setopt extendedglob
+        gemfiles=`ls -d Gemfile^*lock`
+        ;;
+      *bash)
+        gemfiles=`GLOBIGNORE="*.lock" && ls Gemfile*`
+        ;;
+      *)
+        echo "Not sure how to behave with this shell: $local_shell"
+        return 1
+        ;;
+    esac
+
+    bundles=`echo "$gemfiles" | cut -f2 -d"."`
     echo "chbundle  version $CHBUNDLE_VERSION"
     echo "bundles:"
 
-    for bundle in $bundles
+    for bundle in $(echo "$bundles")
     do
+      if [[ "$bundle" == "Gemfile" ]]
+        then
+        echo "- default"
+      else
         echo "- $bundle"
+      fi
     done
 }
 
